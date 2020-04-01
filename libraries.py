c156958565ef6974a7749bc015b21c3b43e5f55b
Alireza <Peyman.majidi@gmail.com>
2020-04-01 13:08:25 +0430

    external blockybock class & global libraries

---
+++ libraries.py
@@ -0,0 +1,45 @@
+import pygame, random, math
+from game_contants import *
+
+class Direction:
+    LEFT = '👈'
+    FRONT = '🐥'
+    RIGHT = '👉'
+
+class Emotion:
+    SAD = '😞'
+    WOW = '😲'
+    HAPPY = '😊'
+
+def play_audio(soundname):
+    pygame.mixer.music.load("sounds/"+soundname)
+    pygame.mixer.music.play(0)
+
+
+class Point:
+    def __init__(self, x=0,y=0):
+        self.x = x
+        self.y = y
+    def set(self, x,y):
+        self.x = x
+        self.y = y
+    def get(self):
+        return [self.x, self.y]
+    def get2(self):
+        return (self.x, self.y)
+
+# How to Play
+def PrintHelpOnConsole():
+    print("""■ Welcome to Blocky Blocky (B.b)
+It is a fight between mouse cursor and Blocky Block !
+
+Use Arrow keys to move around, press [space bar] to jump
+Hit [Enter] to shot laser gun
+Press [+] and [-] keys to make the B.b bigger or smaller
+Finally, [click] anywhere of playground by Mouse to draw anything you like; B.B can move and jump over them
+
+
+■ Other useful trick:
+C: Clear the playground
+G: Green brush
+R: Red brush""")
