9d11db4c97e72eb2217c374b9aad16cee79ca15a
Peyman Majidi <Peyman.majidi@gmail.com>
2020-04-02 11:02:24 +0430

    players list added

--- main.py
+++ main.py
@@ -15,21 +15,21 @@ def main():
     pygame.display.set_caption("Welcome to Blockyblock game | by Peyman")
     paint_color = (0,255,0)
     clicked = False # mouse button down to starting to draw
+    pygame.display.update()
 
     players = list()
 
-    # make a blocky character
-    blocky = BlockyBlock("yellowish blocki", YELLOW) 
-    player2 = BlockyBlock("redish blocki", RED)
-    player2.assign_keystrock(pygame.K_a, pygame.K_d, pygame.K_s,pygame.K_w)
-    player2.x = 100
-    hello = BlockyBlock("hello", WHITE)
-    hello.x = 400
+    blocky_yellow = BlockyBlock("yellowish blocki", YELLOW)
+    blocky_red = BlockyBlock("redish blocki", RED)
+    #customize keystrock
+    blocky_red.assign_keystrock(pygame.K_a, pygame.K_d, pygame.K_s,pygame.K_w)
+    blocky_red.set_x(100)
 
-    players.append(blocky)
-    players.append(player2)
-    players.append(hello)    
+    players.append(blocky_red)
+    players.append(blocky_yellow)
 
+
+    pygame.display.update()
     PrintHelpOnConsole()
     game_over = False
     while not game_over: # Main Game Loop   
@@ -42,19 +42,15 @@ def main():
             p.alive(keys)
         
 
-        if keys[pygame.K_RIGHT]:
-            print(f"peyman.x: {player2.x} blocky.x {blocky.x}\r",end = '')
-
-            if (blocky.x + CHARCTER == player2.x) or (blocky.x + CHARCTER +1 == player2.x):
-                if (blocky.y - CHARCTER <= player2.y-CHARCTER and blocky.y > player2.y-CHARCTER) or ( blocky.y - CHARCTER > player2.y - CHARCTER and blocky.y - CHARCTER < player2.y ):
-                    player2.turn_right()
+        # if keys[pygame.K_RIGHT]:
+        #     if (blocky.x + CHARCTER == player2.x) or (blocky.x + CHARCTER +1 == player2.x):
+        #         if (blocky.y - CHARCTER <= player2.y-CHARCTER and blocky.y > player2.y-CHARCTER) or ( blocky.y - CHARCTER > player2.y - CHARCTER and blocky.y - CHARCTER < player2.y ):
+        #             player2.turn_right()
                         
-        if keys[pygame.K_LEFT]:
-            print(f"peyman.x: {player2.x} blocky.x {blocky.x}\r",end = '')
-
-            if (blocky.x == player2.x+ CHARCTER ) or (blocky.x  == player2.x+ CHARCTER +1):
-                if (blocky.y - CHARCTER <= player2.y-CHARCTER and blocky.y > player2.y-CHARCTER) or ( blocky.y - CHARCTER > player2.y - CHARCTER and blocky.y - CHARCTER < player2.y ):
-                    player2.turn_left()
+        # if keys[pygame.K_LEFT]:
+        #     if (blocky.x == player2.x+ CHARCTER ) or (blocky.x  == player2.x+ CHARCTER +1):
+        #         if (blocky.y - CHARCTER <= player2.y-CHARCTER and blocky.y > player2.y-CHARCTER) or ( blocky.y - CHARCTER > player2.y - CHARCTER and blocky.y - CHARCTER < player2.y ):
+        #             player2.turn_left()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -73,12 +69,13 @@ def main():
 
                 for p in players:
                     p.event(event.key)
+                    
 
                 if event.key == pygame.K_c:
                     pygame.draw.rect(window, BLACK, [0,0,WIDTH, HEIGHT],0)
 
                 if event.key == pygame.K_RSHIFT:
-                    paint_color = (random.randrange(1,255),random.randrange(1,255),random.randrange(1,255))
+                    paint_color = random_color_generator()
         
                 if event.key == pygame.K_b:
                     paint_color = (0,0,0)
@@ -93,13 +90,15 @@ def main():
                     paint_color = (255,255,0)
                     
                 if event.key == pygame.K_KP_PLUS or  event.key ==pygame.K_PLUS or event.key == pygame.K_l:
-                    blocky.zoom_in()
+                    for player in players:
+                        player.zoom_in()
                         
                 if event.key == pygame.K_MINUS or  event.key ==pygame.K_KP_MINUS:
-                    blocky.zoom_out()
+                    for player in players:
+                        player.zoom_out()
         
         pygame.display.update()
-        pygame.time.delay(2)
+        pygame.time.delay(1) # refresh rate
         # end of main loop
 
 if __name__ == "__main__":
