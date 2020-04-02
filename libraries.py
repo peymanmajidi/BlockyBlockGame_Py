71cfcd606c0f4ba14b845382d074987b1d39e8c2
Peyman Majidi <Peyman.majidi@gmail.com>
2020-04-02 12:28:41 +0430

    new help command

--- libraries.py
+++ libraries.py
@@ -37,13 +37,16 @@ def PrintHelpOnConsole():
     print("""■ Welcome to Blocky Blocky (B.b)
 It is a fight between mouse cursor and Blocky Block !
 
-Use Arrow keys to move around, press [space bar] to jump
+Use Arrow keys to move around, press [space bar] to jump: yellow blocky
+Press a-w-d to move other blockies
 Hit [Enter] to shot laser gun
 Press [+] and [-] keys to make the B.b bigger or smaller
 Finally, [click] anywhere of playground by Mouse to draw anything you like; B.B can move and jump over them
 
-
 ■ Other useful trick:
 C: Clear the playground
 G: Green brush
-R: Red brush""")
+R: Red brush
+
+Press [tab] to generate new blocky
+""")
