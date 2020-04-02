082c93fdf1437fe20eb1702ae39548ee63d97177
Peyman Majidi <Peyman.majidi@gmail.com>
2020-04-02 12:43:56 +0430

    fixed unicode error

--- libraries.py
+++ libraries.py
@@ -34,7 +34,7 @@ def random_color_generator():
 
 
 def PrintHelpOnConsole():
-    print("""■ Welcome to Blocky Blocky (B.b)
+    print("""Welcome to Blocky Blocky (B.b)
 It is a fight between mouse cursor and Blocky Block !
 
 Use Arrow keys to move around, press [space bar] to jump: yellow blocky
@@ -43,7 +43,7 @@ Hit [Enter] to shot laser gun
 Press [+] and [-] keys to make the B.b bigger or smaller
 Finally, [click] anywhere of playground by Mouse to draw anything you like; B.B can move and jump over them
 
-■ Other useful trick:
+Other useful trick:
 C: Clear the playground
 G: Green brush
 R: Red brush
