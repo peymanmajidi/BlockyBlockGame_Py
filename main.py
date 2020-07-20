6702decd7242e77b3236111c826dba195a6d930e
Peyman Majidi <peyman.majidi@gmail.com>
2020-07-20 15:09:45 +0430

    demo

--- main.py
+++ main.py
@@ -5,27 +5,32 @@ pygame.init()
 pygame.display.set_caption(GAME_TITLE)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-player1 = BlockyBlock(screen, "player1", color = YELLOW, size= Character_Size.Normal)
+player1 = BlockyBlock(screen, "player1", x=10, color = YELLOW, size= Character_Size.Normal)
 player1.assign_keystrock(STANDARD_INPUT)
 player1.set_as_player1()
 
+
+
+BlockyBlock(screen, x=1000, color = GREEN, size= Character_Size.SuperBig)
+BlockyBlock(screen, x=1150, color = LIGHT_BLUE, size= Character_Size.Big)
+
+
 UI.console_help()
 
-blocky_generator(screen, second=3)
+blocky_generator(screen, second=5)
 pygame.display.update()
 
 while not game_over:
-    UI.level_design(screen)
+    UI.level_01(screen)
 
     BlockyBlock.update(pygame.key.get_pressed())
 
     arrow_keys(screen, pygame.event)
-    mouse_click(screen, pygame.event)
                 
     pygame.display.update()
     pygame.time.delay(REFRESH)
-    BlockyBlock.render_all()
-    game_over = BlockyBlock.game_over
+    game_over = BlockyBlock.render_all()
+
 
 UI.game_over(screen)
 os._exit(0)
