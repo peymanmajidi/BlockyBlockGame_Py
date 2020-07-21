e21d8b5f7e303bf4ef1075c8626a579a05d45f99
Peyman Majidi <peyman.majidi@gmail.com>
2020-07-21 12:40:14 +0430

    new players setup

--- main.py
+++ main.py
@@ -5,20 +5,18 @@ pygame.init()
 pygame.display.set_caption(GAME_TITLE)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
-player1 = BlockyBlock(screen, "player1", x=10, color = YELLOW, size= Character_Size.Normal)
+player1 = BlockyBlock(screen, "player1", x=50, y=600, color = YELLOW, size= Character_Size.Normal)
 player1.assign_keystrock(STANDARD_INPUT)
 player1.set_as_player1()
 
 
 
 
-BlockyBlock(screen, x=950, color = GREEN, size= Character_Size.SuperBig)
-BlockyBlock(screen, x=1200, color = LIGHT_BLUE, size= Character_Size.SuperTiny, automatic= True)
+BlockyBlock(screen, x= 1100, color= GREEN, size= Character_Size.SuperBig)
+BlockyBlock(screen, x= 500, color= LIGHT_BLUE, size= Character_Size.SuperTiny, automatic= True).direction = Direction.LEFT
 
-
-for i in range(1,20):
-    BlockyBlock(screen, x=900-i+ random.randint(-10,10) ,y=i*10, color = random_color_generator(), size= Character_Size.Tiny)
-
+for i in range(1,19):
+    BlockyBlock(screen, x= 900-i+ random.randint(-10,10) ,y=i*10, color = random_color_generator(), size= Character_Size.Tiny)
 
 UI.console_help()
 
